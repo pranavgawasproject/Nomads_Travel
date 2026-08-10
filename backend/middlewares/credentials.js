b299695b1f74e82493d5d98d8ff932e21beef35d
Pranav Gawas <pranavgawas.project@gmail.com>
2026-08-10 14:05:15 +0000

    refactor: remove legacy MongoDB backend server, Mongoose models, and bun.lock

--- backend/middlewares/credentials.js
+++
@@ -1,9 +0,0 @@
-import { allowedOrigins } from "../config/corsConfig.js";
-
-export default function credentials(req, res, next) {
-  const { origin } = req.headers;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Credentials", true);
-  }
-  next();
-}
--- backend/routes/activityFeedRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getActivityFeed, createActivity } from "../controllers/activityFeedController.js";
-
-const router = Router();
-
-router.get("/", getActivityFeed);
-router.post("/", createActivity);
-
-export default router;
